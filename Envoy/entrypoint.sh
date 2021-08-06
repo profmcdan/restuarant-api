9ad653b17afd263b94687226792e565dc432a7ea
Daniel Ale <danielale9291@gmail.com>
2021-08-06 07:25:52 +0100

    add envoy

---
+++ Envoy/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sed -e "s/\${SERVICE_NAME}/${SERVICE_NAME}/" \
+    -e "s/\${SERVICE_PORT}/${SERVICE_PORT}/" \
+    /config/envoy.yaml > /etc/envoy.yaml
+    
+/usr/local/bin/envoy -c /etc/envoy.yml -l ${DEBUG_LEVEL}
